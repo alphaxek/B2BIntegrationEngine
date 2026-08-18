616722fe911fc6fd725b2929eba4c561adefdfd5
Alpha <116849110+alpha-xek@users.noreply.github.com>
2026-08-18 22:08:56 +0530

    initial commit

---
+++ src/main/java/com/engine/B2BIntegrationEngine/B2BIntegrationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.engine.B2BIntegrationEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2BIntegrationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2BIntegrationEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engine/B2BIntegrationEngine/B2BIntegrationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.B2BIntegrationEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2BIntegrationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
